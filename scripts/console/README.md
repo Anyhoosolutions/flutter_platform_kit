1c35927e27e2c0d4b7d425b3dca4887eecbd8a4a
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-19 15:12:32 -0800

    Add script for uploading docs

---
+++ scripts/console/README.md
@@ -0,0 +1,4 @@
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
+
+UPLOAD_URL="http://127.0.0.1:5001/anyhoosolutions/us-central1/uploadDocumentation" dart run
---
+++ scripts/console/bin/console.dart
@@ -0,0 +1,17 @@
+import 'package:console/documentation_uploader/documentation_uploader.dart';
+
+Future<void> main(List<String> arguments) async {
+  String? commitHash;
+  final isBranch = arguments.any((a) => a.startsWith('--commitHash='));
+  if (isBranch) {
+    commitHash = arguments
+        .where((a) => a.startsWith('--commitHash='))
+        .first
+        .split('=')[1];
+  }
+  final documentationUploader = DocumentationUploader(
+    isBranch: isBranch,
+    commitHash: commitHash,
+  );
+  await documentationUploader.upload();
+}
