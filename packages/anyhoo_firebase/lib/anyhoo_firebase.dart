22be4cfc3fce164e91e94aa69668b0706a3f82b6
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-20 08:32:10 -0800

    Generate new package

---
+++ packages/anyhoo_firebase/lib/anyhoo_firebase.dart
@@ -0,0 +1,10 @@
+/// UanyhooUfirebase Package
+///
+/// <Brief description of what the package does>
+library anyhoo_firebase;
+
+export 'src/calculator.dart';
+
+// Add exports for all public API files
+// export 'path/to/public_file1.dart';
+// export 'path/to/public_file2.dart';
