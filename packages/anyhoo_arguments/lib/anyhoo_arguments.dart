0e66e8ac9bd6671b21fbbbf780cc0d644fb5a519
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-20 06:45:08 -0800

    Generate new package

---
+++ packages/anyhoo_arguments/lib/anyhoo_arguments.dart
@@ -0,0 +1,10 @@
+/// UanyhooUarguments Package
+///
+/// <Brief description of what the package does>
+library anyhoo_arguments;
+
+export 'src/calculator.dart';
+
+// Add exports for all public API files
+// export 'path/to/public_file1.dart';
+// export 'path/to/public_file2.dart';
