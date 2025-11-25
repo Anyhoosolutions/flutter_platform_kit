67e04d3e18408e8805d6d8cfd4d89dc3720c8ae0
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-25 14:10:38 -0800

    Remove app bar

--- packages/anyhoo_app_bar/README.md
+++
@@ -1,6 +0,0 @@
-
-# anyhoo_app_bar
-
-A package providing a custom app bar
-It supports some easier way to define action buttons,
-use a background image or just a top bar.
--- packages/anyhoo_app_bar/lib/anyhoo_app_bar.dart
+++
@@ -1,7 +0,0 @@
-/// anyhoo_app_bar Package
-///
-/// A package for creating app bars.
-// ignore: unnecessary_library_name
-library anyhoo_app_bar;
-
-export 'src/anyhoo_app_bar.dart';
