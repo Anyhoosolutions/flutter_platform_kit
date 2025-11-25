2f3d2f976b4103a81cde910f93869490d277a27d
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-25 13:41:10 -0800

    Create package

---
+++ packages/anyhoo_shimmer/lib/anyhoo_shimmer.dart
@@ -0,0 +1,10 @@
+/// UanyhooUshimmer Package
+///
+/// <Brief description of what the package does>
+library anyhoo_shimmer;
+
+export 'src/calculator.dart';
+
+// Add exports for all public API files
+// export 'path/to/public_file1.dart';
+// export 'path/to/public_file2.dart';
