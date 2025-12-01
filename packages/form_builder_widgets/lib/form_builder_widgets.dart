40a8c8acf853ef757f9d4d9642ee5effc48e223f
Andreas Lidholm <lidholm.andreas@gmail.com>
2025-11-30 22:15:29 -0800

    Create package

---
+++ packages/form_builder_widgets/lib/form_builder_widgets.dart
@@ -0,0 +1,7 @@
+/// form_builder_widgets Package
+///
+/// A collection of widgets for form building using Flutter Form Builder.
+// ignore: unnecessary_library_name
+library form_builder_widgets;
+
+export 'src/calculator.dart';
