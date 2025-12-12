5c10c51fab1a06fd80a60b049384b4186076a819
lidholm <lidholm.andreas@gmail.com>
2025-12-12 10:10:31 -0800

    Expose AnyhooStorageService classes (#54)

--- packages/anyhoo_firebase/lib/anyhoo_firebase.dart
+++ packages/anyhoo_firebase/lib/anyhoo_firebase.dart
@@ -6,7 +6,13 @@
 library anyhoo_firebase;
 
 export 'src/config/firebase_initializer.dart';
+export 'src/config/emulator_config.dart';
+export 'src/services/firestore_service.dart';
+export 'src/services/storage/anyhoo_storage_service.dart';
+export 'src/services/storage/anyhoo_firebase_storage_service.dart';
+export 'src/services/storage/anyhoo_fake_storage_service.dart';
+export 'src/services/storage/anyhoo_upload_task.dart';
+export 'src/services/storage/anyhoo_firebase_upload_task.dart';
+export 'src/services/storage/anyhoo_fake_upload_task.dart';
 export 'src/os_tool.dart';
 export 'src/widgets/firebase_analytics_page.dart';
-export 'src/services/firestore_service.dart';
-export 'src/config/emulator_config.dart';
