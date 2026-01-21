169275d8b7f8f29060ea0fde7cb132ac9ac215a0
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-01-21 11:37:54 -0500

    Allow to not use SliverToBoxAdapter for AnyhooAppBar

--- packages/anyhoo_search_bar/lib/src/anyhoo_search_bar.dart
+++ packages/anyhoo_search_bar/lib/src/anyhoo_search_bar.dart
@@ -6,6 +6,7 @@ class AnyhooSearchBar extends StatefulWidget {
   final void Function(bool)? onIncludeEverythingChanged;
   final IconData icon;
   final String labelText;
+  final bool wrapInSliver;
 
   const AnyhooSearchBar({
     super.key,
@@ -14,6 +15,7 @@ class AnyhooSearchBar extends StatefulWidget {
     required this.onChanged,
     this.onIncludeEverythingChanged,
     this.icon = Icons.search,
+    this.wrapInSliver = true,
   });
 
   @override
@@ -39,53 +41,55 @@ class _AnyhooSearchBarState extends State<AnyhooSearchBar> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(color: const Color(0xFFFFF0E6), borderRadius: BorderRadius.circular(80)),
-                child: TextField(
-                  controller: searchController,
-                  onChanged: widget.onChanged,
-                  decoration: InputDecoration(
-                    labelText: widget.labelText,
-                    hintStyle: TextStyle(color: theme.colorScheme.primary, fontSize: 16),
-                    prefixIcon: Icon(widget.icon, color: theme.colorScheme.primary),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    final child = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: const Color(0xFFFFF0E6), borderRadius: BorderRadius.circular(80)),
+              child: TextField(
+                controller: searchController,
+                onChanged: widget.onChanged,
+                decoration: InputDecoration(
+                  labelText: widget.labelText,
+                  hintStyle: TextStyle(color: theme.colorScheme.primary, fontSize: 16),
+                  prefixIcon: Icon(widget.icon, color: theme.colorScheme.primary),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
                   ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                 ),
               ),
             ),
-            if (widget.showIncludeEverythingCheckbox)
-              Checkbox(
-                value: includeEverything,
-                onChanged: (value) {
-                  widget.onIncludeEverythingChanged?.call(value ?? false);
-                  setState(() {
-                    includeEverything = value ?? false;
-                  });
-                },
-              ),
-            if (widget.showIncludeEverythingCheckbox)
-              Text(
-                'Full',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: theme.colorScheme.primary),
-              ),
-          ],
-        ),
+          ),
+          if (widget.showIncludeEverythingCheckbox)
+            Checkbox(
+              value: includeEverything,
+              onChanged: (value) {
+                widget.onIncludeEverythingChanged?.call(value ?? false);
+                setState(() {
+                  includeEverything = value ?? false;
+                });
+              },
+            ),
+          if (widget.showIncludeEverythingCheckbox)
+            Text(
+              'Full',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: theme.colorScheme.primary),
+            ),
+        ],
       ),
     );
+    if (widget.wrapInSliver) {
+      return SliverToBoxAdapter(child: child);
+    }
+    return child;
   }
 }
