19dffedada2243d46ab139e8c28103038e2e6195
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-01-25 17:32:42 -0500

    Sanitized version of filepath instead of random id

--- tools/upload_documentation/lib/documentation_uploader/utils.dart
+++ tools/upload_documentation/lib/documentation_uploader/utils.dart
@@ -4,11 +4,9 @@ import 'package:crypto/crypto.dart';
 
 class Utils {
   static String generateRandomString(int length) {
-    const String chars =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const String chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     Random random = Random();
-    return List.generate(length, (index) => chars[random.nextInt(chars.length)])
-        .join();
+    return List.generate(length, (index) => chars[random.nextInt(chars.length)]).join();
   }
 
   static String hash(String input) {
@@ -20,5 +18,11 @@ class Utils {
 
     // Convert the hash to a hexadecimal string
     return digest.toString().substring(20);
+  }
+
+  /// Returns a sanitized version of [filepath] suitable for use as an id.
+  /// Replaces path separators with `-` and trims trailing slashes for readability.
+  static String sanitizeFilepathForId(String filepath) {
+    return filepath.replaceAll('/', '-').replaceAll(RegExp(r'-+$'), '');
   }
 }
