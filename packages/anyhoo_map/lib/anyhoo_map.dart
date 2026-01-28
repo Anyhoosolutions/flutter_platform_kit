cd171f20be6588cf491e3ef93eccfd42007455a1
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-01-28 09:18:14 -0500

    Create package

---
+++ packages/anyhoo_map/lib/anyhoo_map.dart
@@ -0,0 +1,8 @@
+/// Anyhoo Map Package
+///
+/// A class for using a map widget. Will take a enum telling if to use GoogleMaps or FlutterMap.
+/// Also wraps the [latlong2](https://pub.dev/packages/latlong2) package, with a different name to not clash with the GoogleMaps one.
+// ignore: unnecessary_library_name
+library anyhoo_map;
+
+export 'src/calculator.dart';
