14cb522ef087ececed105291735b2c58e8dea8fa
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-04-02 18:21:25 -0400

    Run create package script

---
+++ packages/widgetbook_wrappers/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+* Create widgetbook_wrappers package
---
+++ packages/widgetbook_wrappers/lib/widgetbook_wrappers.dart
@@ -0,0 +1,5 @@
+export 'src/calculator.dart';
+
+// Add exports for all public API files
+// export 'path/to/public_file1.dart';
+// export 'path/to/public_file2.dart';
