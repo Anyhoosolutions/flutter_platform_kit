26b1e3c2d48bf49b89a61dd851b2af8c0b4f5b29
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-08-11 11:48:07 -0700

    Creating a new scaffold package

---
+++ packages/anyhoo_design_system/lib/anyhoo_design_system.dart
@@ -0,0 +1,10 @@
+/// UanyhooUdesignUsystem Package
+///
+/// <Brief description of what the package does>
+library anyhoo_design_system;
+
+export 'src/calculator.dart';
+
+// Add exports for all public API files
+// export 'path/to/public_file1.dart';
+// export 'path/to/public_file2.dart';
