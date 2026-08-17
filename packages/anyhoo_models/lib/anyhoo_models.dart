f0c00f1842fe0e4356299937a0d9e4bdc0f0f92f
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-08-17 11:37:33 -0700

    Add AnyhooUser model

--- packages/anyhoo_models/lib/anyhoo_models.dart
+++ packages/anyhoo_models/lib/anyhoo_models.dart
@@ -1,10 +1,4 @@
-/// UanyhooUmodels Package
-///
-/// <Brief description of what the package does>
-library anyhoo_models;
+/// Contains the base models for the Anyhoo platform.
+library;
 
-export 'src/calculator.dart';
-
-// Add exports for all public API files
-// export 'path/to/public_file1.dart';
-// export 'path/to/public_file2.dart';
+export 'src/anyhoo_user.dart';
