86def0c362fa674279cd268450ca6b03f252d36d
lidholm <lidholm.andreas@gmail.com>
2026-08-31 17:59:00 +0200

    fix_for_key_on_anyhoo_segment (#169)

--- packages/anyhoo_design_system/lib/src/widgets/forms/anyhoo_segmented_control.dart
+++ packages/anyhoo_design_system/lib/src/widgets/forms/anyhoo_segmented_control.dart
@@ -48,7 +48,7 @@ class AnyhooSegmentedControl<T> extends StatelessWidget {
 }
 
 class _SegmentThumb extends StatelessWidget {
-  const _SegmentThumb({required this.label, required this.selected, required this.onTap});
+  const _SegmentThumb({required this.label, required this.selected, required this.onTap, super.key});
 
   final String label;
   final bool selected;
