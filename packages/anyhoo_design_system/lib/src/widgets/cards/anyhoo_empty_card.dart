6081219876158794b2dac9fd18a49f18d692cfbd
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-09-18 21:39:31 -0700

    Create an empty card

---
+++ packages/anyhoo_design_system/lib/src/widgets/cards/anyhoo_empty_card.dart
@@ -0,0 +1,17 @@
+import 'package:anyhoo_design_system/anyhoo_design_system.dart';
+import 'package:flutter/material.dart';
+
+/// Simple elevated card with title, body, and an optional text action.
+class AnyhooEmptyCard extends StatelessWidget {
+  const AnyhooEmptyCard({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnyhooCardShell(
+      padding: const EdgeInsets.all(DesignTokens.spacingMd),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, mainAxisSize: MainAxisSize.min, children: [child]),
+    );
+  }
+}
