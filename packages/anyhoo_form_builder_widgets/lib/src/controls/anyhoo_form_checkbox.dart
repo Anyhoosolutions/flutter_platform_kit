747481780b11dab49926105d8f1db6a244a89726
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-09-21 09:59:10 -0700

    Add validators as parameters

--- packages/anyhoo_form_builder_widgets/lib/src/controls/anyhoo_form_checkbox.dart
+++ packages/anyhoo_form_builder_widgets/lib/src/controls/anyhoo_form_checkbox.dart
@@ -4,17 +4,18 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:anyhoo_design_system/anyhoo_design_system.dart';
 
 class AnyhooFormCheckbox extends StatelessWidget {
-  const AnyhooFormCheckbox({super.key, required this.name, required this.label});
+  const AnyhooFormCheckbox({super.key, required this.name, required this.label, this.validators});
 
   final String label;
   final String name;
+  final List<FormFieldValidator<bool>>? validators;
 
   @override
   Widget build(BuildContext context) {
     return FormBuilderField(
       initialValue: false,
       name: name,
-      validator: FormBuilderValidators.compose([FormBuilderValidators.required()]),
+      validator: FormBuilderValidators.compose(validators ?? <FormFieldValidator<bool>>[]),
       builder: (FormFieldState<bool> field) {
         final isSelected = field.value ?? false;
 
--- packages/anyhoo_form_builder_widgets/lib/src/controls/anyhoo_form_radio_group.dart
+++ packages/anyhoo_form_builder_widgets/lib/src/controls/anyhoo_form_radio_group.dart
@@ -6,17 +6,18 @@ import 'package:anyhoo_design_system/anyhoo_design_system.dart';
 typedef Label = String;
 
 class AnyhooFormRadioGroup<T> extends StatelessWidget {
-  const AnyhooFormRadioGroup({super.key, required this.name, required this.options});
+  const AnyhooFormRadioGroup({super.key, required this.name, required this.options, this.validators});
 
   final String name;
   final List<(T, Label)> options;
+  final List<FormFieldValidator<T>>? validators;
 
   @override
   Widget build(BuildContext context) {
     return FormBuilderField<T>(
       initialValue: options.first.$1,
       name: name,
-      validator: FormBuilderValidators.compose([FormBuilderValidators.required()]),
+      validator: FormBuilderValidators.compose(validators ?? <FormFieldValidator<T>>[]),
       builder: (FormFieldState<T> field) {
         return Wrap(
           children: options.map((o) => _radio(field.value, o.$1, o.$2, (T? t) => field.didChange(t))).toList(),
